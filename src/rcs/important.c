27595ad3a5feb688d0fce70feb81b25f5a5e93f8
Pabitra Adhikari <pabitraadhikari@gmail.com>
2014-04-17 12:24:46 -0700

    cvs version control

---
+++ src/rcs/important.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+	
+	/*
+		This is an important file for managing this project.
+		It implements the canonical “Hello World” program.
+	*/
+
+	printf("Hello World\n");
+	printf("This is an extra line added later\n");
+	exit(EXIT_SUCCESS);
+}
+
